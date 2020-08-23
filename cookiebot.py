a5bbc2bc3a8f9dcd93f5c4bc12608e79875b2e19
balveren <65164774+balveren@users.noreply.github.com>
2020-08-23 11:05:40 +0300

    Update cookiebot.py

--- cookiebot.py
+++ cookiebot.py
@@ -3,27 +3,26 @@ from selenium import webdriver
 from termcolor import colored
 import time
 
-#internet check durumu eklenecek
-
 sorgu = input("Enter url (Ex: https:google.com):\n")
 sorgu = sorgu.split()
+
 #importing driver and running in the background
 option = webdriver.ChromeOptions()
 option.headless = True
 driver = webdriver.Chrome(options=option)
 
 
-Anaurl = "https://www.cookieserve.com"
-driver.get(Anaurl)
+mainurl = "https://www.cookieserve.com"
+driver.get(mainurl)
 time.sleep(2)
 
 url_kısım = driver.find_element_by_class_name("form-control").send_keys(sorgu)
-buton = driver.find_element_by_id("scan_cookie").click()
+button = driver.find_element_by_id("scan_cookie").click()
 time.sleep(5)
 
-tablo = driver.find_element_by_css_selector("#CookieTable")
+table = driver.find_element_by_css_selector("#CookieTable")
 
-for row in tablo.find_elements_by_css_selector('tbody'):
+for row in table.find_elements_by_css_selector('tbody'):
     print("\n")
     print(colored("                                           Cookie Bot 0.4         ","red",attrs=['bold']))
     for cell in row.find_elements_by_tag_name('tr'):
@@ -35,9 +34,7 @@ for row in tablo.find_elements_by_css_selector('tbody'):
             for sutun in cell.find_elements_by_tag_name('td'):
                 print(colored(liste[i],"red"),":",sutun.text)
                 i += 1
-#Özet kısmı eklenecek toplam şu kadar  bulunud gdpr ve kvkk uygunluk durumu gibi
-# rapor olarak word e kaydedilecek
-# Quit kısmı eklenecek
+
 
 time.sleep(10)
 driver.close()
